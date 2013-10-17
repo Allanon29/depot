b449ee5e7c39cd2546e8d1368043c9a0cde07cfc
Mate Kaszas <kaszas.mate@upcmail.hu>
2013-10-17 15:38:37 +0200

    Added header adn sidebar to application.html.erb with custom styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Depot::Application.routes.draw do
+  root 'store#index', as: 'store'
+
+  get "store/index"
   resources :products
 
   # The priority is based upon order of creation: first created -> highest priority.
